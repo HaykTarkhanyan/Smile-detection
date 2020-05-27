9c6624d354cda4dc641743fc7ac4aa36d0f4f017
HaykTarkhanyan <hayk_tarkhanyan@outlook.com>
2020-05-27 14:05:45 +0400

    Summart (required)

--- run.py
+++ run.py
@@ -29,7 +29,7 @@ while not done:
     ret, img = cap.read()
 
     if ret:
-        frame_to_display = img.copy()
+        frame_to_display = img
         frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # Detect faces in the image
@@ -42,8 +42,8 @@ while not done:
 
         if len(faces) > 0:
             # Draw a rectangle around the faces
-            # for (x, y, w, h) in faces:
-            #     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
             frame_tmp = img[faces[0][1]:faces[0][1] + faces[0]
                             [3], faces[0][0]:faces[0][0] + faces[0][2]:1, :]
@@ -72,8 +72,7 @@ while not done:
                     frame_to_display[:emoji.shape[0], :emoji.shape[1]] = emoji
 
 
-            cv2.imwrite("cropped_face.png", frame_cropped)
-            cv2.imshow('Face Recognition', frame_to_display)
+            cv2.imshow('Jpitik detection', frame_to_display)
 
         waitkey = cv2.waitKey(1)
         if waitkey == ord('q') or waitkey == ord('Q'):
