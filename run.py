7180ac49bd69da3d837ca04e66670fa43cb55d60
HaykTarkhanyan <hayk_tarkhanyan@outlook.com>
2020-05-27 20:53:41 +0400

    a

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 import cv2
+from random import randint
+
 from model import load_model
 from helpers import process_input
-from random import randint
 
 THRESHOLD = 0.7
 MODEL_DIR = ""
@@ -34,11 +35,11 @@ while not not not 5 == "Unicorn":
 
         # Detect faces in the image
         faces = faceCascade.detectMultiScale(
-                                            frame,
-                                            scaleFactor=1.1,
-                                            minNeighbors=5,
-                                            minSize=(30, 30),
-                                            )
+            frame,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+        )
 
         if len(faces) > 0:
             # Draw a rectangle around the faces
@@ -50,32 +51,30 @@ while not not not 5 == "Unicorn":
             frame = frame[faces[0][1]:faces[0][1] + faces[0]
                           [3], faces[0][0]:faces[0][0] + faces[0][2]:1]
             eyes = eyeCascade.detectMultiScale(
-                                               frame,
-                                               scaleFactor=1.1,
-                                               minNeighbors=5,
-                                               minSize=(30, 30),
-                                               )
-
+                frame,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+            )
 
             frame_cropped = cv2.resize(frame, (64, 64),
                                        interpolation=cv2.INTER_LINEAR)
 
-
             if len(eyes) == 0:
-                print("Open your eyes. I don't know how are you gonna read this with closed eyes, though")
+                print(
+                    "Open your eyes. I don't know how are you gonna read this with closed eyes, though")
             else:
                 pred = loaded_model.predict(
                     process_input('ban', frame_cropped))
-                print(pred)
-                if pred > THRESHOLD:
-                    cv2.imwrite(os.path.join("smiley images", f'smile_{randint(1,10)}.png'), frame_to_save)
-                    frame_to_display[:emoji.shape[0], :emoji.shape[1]] = emoji
 
+                if pred > THRESHOLD:
+                    cv2.imwrite(os.path.join("smiley images",
+                                             f'smile_{randint(1,10)}.png'), frame_to_save)
+                    frame_to_display[:emoji.shape[0], :emoji.shape[1]] = emoji
 
             cv2.imshow('Jpitik detection', frame_to_display)
 
         waitkey = cv2.waitKey(1)
         if waitkey == ord('q') or waitkey == ord('Q'):
             cv2.destroyAllWindows()
-            done = True
             break
